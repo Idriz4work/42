38c8a3970e49259a89d9b690a46c93bc97defc38
Atilla Idris <136346643+Idriz4work@users.noreply.github.com>
2024-11-01 15:30:26 +0100

    [NEW]
    working but needs to be 1 line shorter in ft_split function

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -1,5 +1,112 @@
+#include "libft.h"
+#include <stdio.h>
+
+int wordCount(char const *s, char c)
+{
+	int i,j;
+
+	i = j = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			j++;
+		i++;
+	}
+	return j;
+}
+
+char **memAlocator(char const *s, char c)
+{
+ int counterofC;
+ char **array;
+
+ counterofC = wordCount(s,c);
+ array = (char **)malloc(sizeof(char *) * (counterofC + 1));
+ if (!array)
+  return NULL;
+
+ array[0] = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+ if (!array[0])
+ {
+  free(array);
+  return NULL;
+ }
+
+ return array;
+}
+
+// char **copy(char **array,int j, char const *s, char c, int i)
+// {
+// 	int counterA;
+// 	counterA = 0;
+// 	array[j] = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+//  if (!array[j])
+// 		return NULL;
+// 	while (s[i] && s[i] != c)
+// 		array[j][counterA++] = s[i++];
+// 	array[j++][counterA] = '\0';
+// 	return array;
+// }
 
 char **ft_split(char const *s, char c)
 {
-	
+	int i,j,counterA;
+	char **array;
+
+	i = counterA = j = 0;
+	array = memAlocator(s,c);
+	if (!array)
+		return NULL;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+		{
+			counterA = 0;
+			array[j] = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+ 		if (!array[j])
+				return NULL;
+			while (s[i] && s[i] != c)
+				array[j][counterA++] = s[i++];
+			array[j++][counterA] = '\0';
+		}
+		else
+			i++;
+	}
+	array[j] = NULL;
+	return array;
+}
+
+int main()
+{
+    char s[] = "Hello , my name is Ati, Nice to see you !";
+    char c = ',';
+    char **result;
+    int j = 0;
+
+    // Call ft_split
+    result = ft_split(s, c);
+    
+    // Check if split was successful
+    if (result == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    // Print each word
+    while (result[j] != NULL)
+    {
+        printf("%s || ", result[j]);
+        j++;
+    }
+
+    // Free allocated memory
+    j = 0;
+    while (result[j] != NULL)
+    {
+        free(result[j]);
+        j++;
+    }
+    free(result);
+
+    return 0;
 }
