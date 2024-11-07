377c9783fe9ecc0b1223c0d643387e44c249cb90
Atilla Idris <136346643+Idriz4work@users.noreply.github.com>
2024-11-07 16:26:48 +0100

    Add files via upload

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -2,5 +2,36 @@
 
 void ft_lstiter(t_list *lst, void (*f)(void *))
 {
+	int i;
 
+	i = 0;
+	while (lst->next != NULL)
+	{
+		(*f)(&lst->content);
+		(*f)(&lst->next);
+	}
+}
+
+#include <stdio.h>
+void convert(t_list *data)
+{
+	printf("function was called");
+}
+
+int main()
+{
+	char *data = "Hello, world!";
+	t_list *node = ft_lstnew(data);
+
+	if (node)
+	{
+		printf("Content: %s\n", (char *)node->content); // Print content of node
+		printf("Next: %p\n", (void *)node->next);       // Should be NULL
+		free(node); // Free allocated memory for node (but not content)
+	}
+	else
+		printf("Node creation failed.\n");
+	ft_lstiter(node,convert);
+
+	return 0;
 }
