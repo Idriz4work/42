c19b06a31053679e413c6672faf50c98c9d668fe
Atilla Idris <136346643+Idriz4work@users.noreply.github.com>
2024-11-20 17:46:35 +0100

    Add files via upload

--- Born2beroot/monitoring.sh
+++ Born2beroot/monitoring.sh
@@ -1,2 +1,26 @@
-!#/shell
+#!/bin/sh
+
+#virtual processors
+cat /proc/cpuinfo | grep '^processor' | wc -l
+
+# physical processors
+cat /proc/cpuinfo | grep 'physical id' | sort -u | wc -l
+
+# date of last reboot
+last reboot | head -1
+
+# for OS and kernel version
+uname -a
+
+# display number of active connections
+ip link show
+
+# display users of server
+who -H -u
+
+# list number of sudo usage
+sudo grep sudo /var/log/auth.log
+
+# The current available storage and its utilization rate as a percentage
+df -BM -i
 
