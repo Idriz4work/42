a314522b611313c729be15ed283dfd60899a3ecd
Atilla Idris <136346643+Idriz4work@users.noreply.github.com>
2025-01-02 02:52:56 +0100

    Update monitoring.sh

--- Born2beroot/monitoring.sh
+++ Born2beroot/monitoring.sh
@@ -1,26 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-#virtual processors
-cat /proc/cpuinfo | grep '^processor' | wc -l
+# Collecting system information
+ARCH=$(uname -a)
+CPU_PHYSICAL=$(grep "physical id" /proc/cpuinfo | sort -u | wc -l)
+VCPU=$(grep -c "processor" /proc/cpuinfo)
+MEMORY=$(free -m | awk 'NR==2 {printf "%s/%sMB (%.2f%%)", $3, $2, $3/$2 * 100}')
+DISK=$(df -h --total | grep total | awk '{printf "%s/%s (%s)", $3, $2, $5}')
+CPU_LOAD=$(top -bn1 | grep "Cpu(s)" | awk '{printf "%.1f%%", $2 + $4}')
+LAST_BOOT=$(who -b | awk '{print $3 " " $4}')
+LVM=$(lsblk | grep -q "lvm" && echo "yes" || echo "no")
+TCP_CONN=$(netstat -an | grep ESTABLISHED | wc -l)
+USER_COUNT=$(who | wc -l)
+IPV4=$(ip a | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}')
+MAC=$(ip link show | grep "link/ether" | awk '{print $2}')
+SUDO_CMDS=$(grep -c "COMMAND" /var/log/sudo.log)
 
-# physical processors
-cat /proc/cpuinfo | grep 'physical id' | sort -u | wc -l
-
-# date of last reboot
-last reboot | head -1
-
-# for OS and kernel version
-uname -a
-
-# display number of active connections
-ip link show
-
-# display users of server
-who -H -u
-
-# list number of sudo usage
-sudo grep sudo /var/log/auth.log
-
-# The current available storage and its utilization rate as a percentage
-df -BM -i
-
+# Displaying the information using wall
+wall <<EOF
+#Architecture: $ARCH
+#CPU physical: $CPU_PHYSICAL
+#vCPU: $VCPU
+#Memory Usage: $MEMORY
+#Disk Usage: $DISK
+#CPU load: $CPU_LOAD
+#Last boot: $LAST_BOOT
+#LVM use: $LVM
+#Connections TCP: $TCP_CONN ESTABLISHED
+#User log: $USER_COUNT
+#Network: IP $IPV4 ($MAC)
+#Sudo: $SUDO_CMDS cmd
+EOF
